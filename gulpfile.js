9484aeba9de6d1767c28175abf213595862861ca
Aznaur <aznaur.khapchaev@gmail.com>
2019-12-20 14:09:04 +0300

    Правки

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 var gulp = require("gulp");
 var plumber = require("gulp-plumber");
@@ -7,32 +7,40 @@ var sass = require("gulp-sass");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var server = require("browser-sync").create();
+var csso = require("gulp-csso");
+var rename = require("gulp-rename");
+var imagemin = require("gulp-imagemin");
+var webp = require("gulp-webp");
+var svgstore = require("gulp-svgstore")
+var posthtml = require("gulp-posthtml");
+var include = require("posthtml-include");
+var del = require("del");
 
 
-gulp.task('css', function () {
-  return gulp.src('source/sass/style.scss')
+gulp.task("css", function () {
+  return gulp.src("source/sass/style.scss")
     .pipe(plumber())
     .pipe(sourcemap.init())
     .pipe(sass())
     .pipe(postcss([
       autoprefixer()
     ]))
-    .pipe(sourcemap.write('.'))
-    .pipe(gulp.dest('source/css'))
+    .pipe(sourcemap.write("."))
+    .pipe(gulp.dest("source/css"))
     .pipe(server.stream());
 });
 
-gulp.task('server', function () {
+gulp.task("server", function () {
   server.init({
-    server: 'source/',
+    server: "source/",
     notify: false,
     open: true,
     cors: true,
     ui: false
   });
 
-  gulp.watch('source/sass/**/*.{scss,sass}', gulp.series('css'));
-  gulp.watch('source/*.html').on('change', server.reload);
+  gulp.watch("source/sass/**/*.{scss,sass}", gulp.series("css"));
+  gulp.watch("source/*.html").on("change", server.reload);
 });
 
-gulp.task('start', gulp.series('css', 'server'));
+gulp.task("start", gulp.series("css", "server"));
